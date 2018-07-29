8c1c4281c46a395f890eb566f488534453a58b7a
Sampoorna Krishnan, Dinesh Kumar (ds037n) <ds037n@att.com>
2018-07-29 16:26:41 +0530

    First Commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hi! Welcome to ReactJS session!!!</div>
+    );
+  }
+}
+
+export default App;
